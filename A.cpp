7f80291d9d810e9e509ba6d344661549dc36f45d
kusano <kusano@users.noreply.github.com>
2021-09-10 14:23:48 +0900

    移動先のみを焼き鈍し
    23005971

--- A.cpp
+++ A.cpp
@@ -5,6 +5,7 @@
 #include <cassert>
 #include <chrono>
 #include <cmath>
+#include <queue>
 using namespace std;
 
 const int N = 16;
@@ -15,14 +16,23 @@ struct RC
 {
     int r, c;
     RC(int r, int c): r(r), c(c) {}
+    bool operator==(const RC &rc) const {return r==rc.r && c==rc.c;}
 };
 
-struct Move
+//  Phase 1用
+struct Move1
+{
+    RC rc;
+    Move1(RC rc): rc(rc) {}
+};
+
+//  Phase 2用
+struct Move2
 {
     //  購入はrc1.r=-1
     //  パスはrc2.r=-1
     RC rc1, rc2;
-    Move(RC rc1, RC rc2): rc1(rc1), rc2(rc2) {}
+    Move2(RC rc1, RC rc2): rc1(rc1), rc2(rc2) {}
 };
 
 vector<RC> SRC[T];
@@ -37,12 +47,31 @@ int xor64(void) {
     return int(x&0x7fffffff);
 }
 
-int calc_k_sub(bool machine[N][N], int r, int c, vector<RC> *V)
+//  隣接しているマシン数を返す
+int calc_k_sub(bool machine[N][N], int r, int c, vector<RC> *hist);
+
+int calc_k(bool machine[N][N], int r, int c)
+{
+    assert(machine[r][c]);
+
+    static vector<RC> hist;
+    int n = calc_k_sub(machine, r, c, &hist);
+
+    while (!hist.empty())
+    {
+        machine[hist.back().r][hist.back().c] = true;
+        hist.pop_back();
+    }
+
+    return n;
+}
+
+int calc_k_sub(bool machine[N][N], int r, int c, vector<RC> *hist)
 {
     assert(machine[r][c]);
 
     machine[r][c] = false;
-    V->push_back(RC(r, c));
+    hist->push_back(RC(r, c));
 
     static int dr[] = {-1, 1, 0, 0};
     static int dc[] = {0, 0, -1, 1};
@@ -54,28 +83,125 @@ int calc_k_sub(bool machine[N][N], int r, int c, vector<RC> *V)
         if (0<=tr && tr<N &&
             0<=tc && tc<N &&
             machine[tr][tc])
-            n += calc_k_sub(machine, tr, tc, V);
+            n += calc_k_sub(machine, tr, tc, hist);
     }
     return n;
 }
 
-int calc_k(bool machine[N][N], int r, int c)
+//  Phase 1
+//  move[i].rcに置く
+//  購入できる資金があるなら買う
+//  資金が無いなら最も古いマシンを移動
+//  マシンの購入金額を引かず、総獲得資金を返す
+long long calc_score1(const vector<Move1> &moves)
 {
-    assert(machine[r][c]);
+    int vege[N][N] = {};
+    bool machine[N][N] = {};
+    long long money = 1;
+    long long money_machine = 0;
+    int j = 0;
+    queue<RC> Q;
 
-    static vector<RC> V;
-    int n = calc_k_sub(machine, r, c, &V);
-
-    while (!V.empty())
+    for (int t=0; t<T; t++)
     {
-        machine[V.back().r][V.back().c] = true;
-        V.pop_back();
+        const RC &rc = moves[t].rc;
+        if (machine[rc.r][rc.c])
+            return -1;
+
+        int m = (j+1)*(j+1)*(j+1);
+        if (m<=money)
+        {
+            j++;
+            money -= m;
+            money_machine += m;
+        }
+        else
+        {
+            RC f = Q.front();
+            Q.pop();
+            machine[f.r][f.c] = false;
+        }
+
+        Q.push(rc);
+        machine[rc.r][rc.c] = true;
+
+        for (int i=0; i<(int)SRC[t].size(); i++)
+            vege[SRC[t][i].r][SRC[t][i].c] = SV[t][i];
+
+        for (int r=0; r<N; r++)
+            for (int c=0; c<N; c++)
+                if (vege[r][c]>0 && machine[r][c])
+                {
+                    int v = vege[r][c];
+                    vege[r][c] = 0;
+                    int k = calc_k(machine, r, c);
+                    money += (long long)v*k;
+                }
+
+        for (RC rc: ERC[t])
+            vege[rc.r][rc.c] = 0;
     }
 
-    return n;
+    return money + money_machine;
 }
 
-long long calc_score(const vector<Move> &moves)
+vector<Move2> convert_moves(const vector<Move1> &moves)
+{
+    int vege[N][N] = {};
+    bool machine[N][N] = {};
+    long long money = 1;
+    int j = 0;
+    queue<RC> Q;
+    vector<Move2> result;
+
+    for (int t=0; t<T; t++)
+    {
+        const RC &rc = moves[t].rc;
+        if (machine[rc.r][rc.c])
+        {
+            result.resize(T, Move2(RC(-1, -1), RC(-1, -1)));
+            return result;
+        }
+
+        int m = (j+1)*(j+1)*(j+1);
+        if (m<=money)
+        {
+            j++;
+            money -= m;
+            result.push_back(Move2(RC(-1, -1), rc));
+        }
+        else
+        {
+            RC f = Q.front();
+            Q.pop();
+            machine[f.r][f.c] = false;
+            result.push_back(Move2(f, rc));
+        }
+
+        Q.push(rc);
+        machine[rc.r][rc.c] = true;
+
+        for (int i=0; i<(int)SRC[t].size(); i++)
+            vege[SRC[t][i].r][SRC[t][i].c] = SV[t][i];
+
+        for (int r=0; r<N; r++)
+            for (int c=0; c<N; c++)
+                if (vege[r][c]>0 && machine[r][c])
+                {
+                    int v = vege[r][c];
+                    vege[r][c] = 0;
+                    int k = calc_k(machine, r, c);
+                    money += (long long)v*k;
+                }
+
+        for (RC rc: ERC[t])
+            vege[rc.r][rc.c] = 0;
+    }
+
+    return result;
+}
+
+long long calc_score2(const vector<Move2> &moves)
 {
     int vege[N][N] = {};
     bool machine[N][N] = {};
@@ -146,9 +272,12 @@ int main()
 
     chrono::system_clock::time_point start = chrono::system_clock::now();
 
-    vector<Move> moves(T, Move(RC(-1, -1), RC(-1, -1)));
+    vector<Move1> moves;
+    for (int i=0; i<T; i++)
+        moves.push_back(RC(i/N%N, i%N));
+
     long long score = 1;
-    vector<Move> best_moves = moves;
+    vector<Move1> best_moves = moves;
     long long best_score = score;
     double time = 0;
     double temp = 1e10;
@@ -158,29 +287,17 @@ int main()
         if (iter%0x100==0)
         {
             chrono::system_clock::time_point now = chrono::system_clock::now();
-            time = chrono::duration_cast<chrono::microseconds>(now-start).count()*1e-6/1.8;
+            time = chrono::duration_cast<chrono::microseconds>(now-start).count()*1e-6/1.5;
             if (time>=1.)
                 break;
             temp = (1-time)*1000;
         }
 
         int t = xor64()%T;
-        Move old_move = moves[t];
-        switch (xor64()%3)
-        {
-        case 0:
-            moves[t].rc1.r = -1;
-            moves[t].rc2 = RC(xor64()%N, xor64()%N);
-            break;
-        case 1:
-            moves[t].rc1 = RC(xor64()%N, xor64()%N);
-            moves[t].rc2 = RC(xor64()%N, xor64()%N);
-            break;
-        case 2:
-            moves[t].rc2.r = -1;
-        }
+        Move1 old_move = moves[t];
+        moves[t].rc = RC(xor64()%N, xor64()%N);
 
-        long long score2 = calc_score(moves);
+        long long score2 = calc_score1(moves);
         if (score2<0)
         {
             moves[t] = old_move;
@@ -201,7 +318,45 @@ int main()
             moves[t] = old_move;
     }
 
-    for (Move move: moves)
+    cerr<<"time: "<<time<<endl;
+    cerr<<"iter: "<<iter<<endl;
+    cerr<<"best_score: "<<best_score<<endl;
+
+    vector<Move2> moves2 = convert_moves(best_moves);
+    vector<Move2> best_moves2 = moves2;
+    long long best_score2 = calc_score2(moves2);
+    while (true)
+    {
+        //  最後の新規購入を探す
+        int t;
+        for (t=T-1; t>=0; t--)
+            if (moves2[t].rc2.r!=-1 && moves2[t].rc1.r==-1)
+                break;
+        if (t<0)
+            break;
+        //  このマシンをパスに変えていく
+        RC rc = moves2[t].rc2;
+        moves2[t].rc1 = RC(-1, -1);
+        moves2[t].rc2 = RC(-1, -1);
+        for (t++; t<T; t++)
+            if (moves2[t].rc1==rc)
+            {
+                rc = moves2[t].rc2;
+                moves2[t].rc1 = RC(-1, -1);
+                moves2[t].rc2 = RC(-1, -1);
+            }
+        long long score = calc_score2(moves2);
+        //cerr<<score<<endl;
+        if (score>best_score2)
+        {
+            best_score2 = score;
+            best_moves2 = moves2;
+        }
+    }
+
+    cerr<<"best_score2: "<<best_score2<<endl;
+
+    for (Move2 move: best_moves2)
     {
         if (move.rc2.r>=0)
             if (move.rc1.r<0)
@@ -211,8 +366,4 @@ int main()
         else
             cout<<-1<<endl;
     }
-
-    cerr<<"time: "<<time<<endl;
-    cerr<<"iter: "<<iter<<endl;
-    cerr<<"best_score: "<<best_score<<endl;
 }
